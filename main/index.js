ad0038895fc52c0645319ae7d89594013b545930
farhanacsebd <farhana.cse.bd@gmail.com>
2023-02-14 23:59:43 +0600

    added setitem

--- main/index.js
+++ main/index.js
@@ -1,72 +1,79 @@
-const allCarts = document.getElementsByClassName('add-cart');
+const carts = document.getElementsByClassName("add-cart");
 
 let products = [
-    {
-        Company: 'Bata',
-        price:20,
-        inCart:0
-    },
-    {
-        Company: 'Apex',
-        price:35,
-        inCart:0
-    },
-    {
-        Company: 'Bee',
-        price:25,
-        inCart:0
-    },
-    {
-        Company: 'Nike',
-        price:15,
-        inCart:0
-    },
-    {
-        Company: 'Reebok',
-        price:55,
-        inCart:0
-    },
-    {
-        Company: 'Sneakers',
-        price:75,
-        inCart:0
-    },
-    {
-        Company: 'Vellore',
-        price:35,
-        inCart:0
-    }
-]
+  {
+    company: "Bata",
+    price: 20,
+    inCart: 0,
+  },
+  {
+    company: "Apex",
+    price: 35,
+    inCart: 0,
+  },
+  {
+    company: "Bee",
+    price: 25,
+    inCart: 0,
+  },
+  {
+    company: "Nike",
+    price: 15,
+    inCart: 0,
+  },
+  {
+    company: "Reebok",
+    price: 55,
+    inCart: 0,
+  },
+  {
+    company: "Sneakers",
+    price: 75,
+    inCart: 0,
+  },
+  {
+    company: "Vellore",
+    price: 35,
+    inCart: 0,
+  },
+];
 
-for (const carts of allCarts) {
-     carts.addEventListener('click',() => {
-        // console.log('hello');
-        cartNumber()
-     })  
+for (let i = 0; i < carts.length; i++) {
+  carts[i].addEventListener("click", () => {
+    cartNumbers(products[i]);
+  });
 }
 
+const onLoadCartNumbers = () => {
+  let productNumbers = localStorage.getItem("cartNumbers");
+  if (productNumbers) {
+    document.querySelector(".cart span").textContent = productNumbers;
+  }
+};
 
-const onLoadCartNumbers = () =>{
-    let productNumbers = localStorage.getItem('cartNumbers');
-    if(productNumbers){
-      document.querySelector('.cart span').textContent = productNumbers;
-    }
+const cartNumbers = (products) => {
+//   console.log("the product click", products);
+
+  let productNumbers = localStorage.getItem("cartNumbers");
+  productNumbers = parseInt(productNumbers);
+  /* console.log(typeof productNumbers); */
+
+  if (productNumbers) {
+    localStorage.setItem("cartNumbers", productNumbers + 1);
+    document.querySelector(".cart span").textContent = productNumbers + 1;
+  } 
+  else {
+    localStorage.setItem("cartNumbers", 1);
+    document.querySelector(".cart span").textContent = 1;
+  }
+  
+  setItem(products)
+};
+
+
+const setItem = (products) =>{
+    console.log('insite the setItem fuction');
+    console.log("the product click", products);
 }
 
-
-const cartNumber = () =>{
-    let productNumbers = localStorage.getItem('cartNumbers');
-    productNumbers = parseInt(productNumbers);
-    /* console.log(typeof productNumbers); */
-
-    if(productNumbers){
-        localStorage.setItem('cartNumbers',productNumbers + 1);
-        document.querySelector('.cart span').textContent = productNumbers + 1;
-    }
-    else{
-        localStorage.setItem('cartNumbers', 1);
-        document.querySelector('.cart span').textContent = 1;
-    }
-}
-
-onLoadCartNumbers()
+onLoadCartNumbers();
